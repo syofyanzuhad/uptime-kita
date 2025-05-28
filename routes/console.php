ed348ba34e160208ae6525c15af4ad2703d52361
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-05-28 08:42:37 +0700

    Add scheduled commands for uptime and certificate checks in console routes

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Spatie\UptimeMonitor\Commands\CheckUptime;
+use Spatie\UptimeMonitor\Commands\CheckCertificates;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command(CheckUptime::class)->everyMinute();
+Schedule::command(CheckCertificates::class)->daily();
