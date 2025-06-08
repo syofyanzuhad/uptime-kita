d6ace796c1799ccbc70311737da0b41e69498c1a
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-06-08 22:40:55 +0700

    Add custom Monitor model extending Spatie's Uptime Monitor and update configuration to use the new model.

---
+++ app/Models/Monitor.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\UptimeMonitor\Models\Monitor as SpatieMonitor;
+
+class Monitor extends SpatieMonitor
+{
+    protected $casts = [
+        'url' => 'string',
+        'uptime_last_check_date' => 'datetime',
+        'certificate_expiration_date' => 'datetime',
+    ];
+
+    protected $fillable = [
+        'url',
+        'uptime_check_enabled',
+        'certificate_check_enabled',
+    ];
+
+    public function getUrlStringAttribute()
+    {
+        return $this->attributes['url'] ?? '';
+    }
+}
