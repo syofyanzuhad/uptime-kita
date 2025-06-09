881cacd8df7388227cb873bce6bde3f7aa88393f
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-06-09 09:29:49 +0700

    Update button element in Edit.vue to use cursor-pointer class for improved user experience

--- resources/js/pages/uptime/Edit.vue
+++ resources/js/pages/uptime/Edit.vue
@@ -83,7 +83,7 @@ const submit = () => {
             </div>
 
             <div class="flex items-center justify-end mt-4">
-              <button type="submit" :class="{ 'opacity-25': form.processing }" :disabled="form.processing" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 active:bg-gray-900 dark:active:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+              <button type="submit" :class="{ 'opacity-25': form.processing }" :disabled="form.processing" class="inline-flex cursor-pointer items-center px-4 py-2 bg-gray-800 dark:bg-gray-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 active:bg-gray-900 dark:active:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                 Perbarui Monitor
               </button>
             </div>
