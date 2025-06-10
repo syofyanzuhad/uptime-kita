d5e1c20d2b193835a0cccab7e1c0f07aa0284021
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-06-10 15:41:05 +0700

    Update footer links in AppSidebar component to point to the new GitHub repository and documentation for the Uptime Monitor project

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -19,12 +19,12 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
+        href: 'https://github.com/syofyanzuhad/uptime-monitor',
         icon: Folder,
     },
     {
         title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
+        href: 'https://github.com/syofyanzuhad/uptime-monitor/blob/main/README.md',
         icon: BookOpen,
     },
 ];
