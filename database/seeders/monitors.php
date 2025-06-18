45b077e5045ba2c581114bc7d455751aa7b79d64
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-06-18 12:29:35 +0700

    Add monitors seeder with predefined URLs and settings
    - Created a new seeder file for monitors with a list of URLs.
    - Each entry includes uptime check and certificate check settings enabled.

---
+++ database/seeders/monitors.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+    [
+        'url' => 'https://www.google.com',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://facebook.com',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://x.com',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://instagram.com',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://linkedin.com',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://youtube.com',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://reddit.com',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://github.com',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://stackoverflow.com',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://medium.com',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://dev.to',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://chatgpt.com',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://openai.com',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://claude.ai',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://perplexity.ai',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+    [
+        'url' => 'https://grok.com',
+        'uptime_check_enabled' => true,
+        'certificate_check_enabled' => true,
+    ],
+];
