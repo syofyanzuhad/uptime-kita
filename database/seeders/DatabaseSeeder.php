784ec275eeb7fa87ffdb22d35838243dd8c915e8
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-06-18 12:30:13 +0700

    Refactor database seeders to include MonitorSeeder and update user creation
    - Introduced MonitorSeeder to seed monitor data with uptime check settings.
    - Updated DatabaseSeeder to create a user with specific email and password using firstOrCreate method.
    - Removed the previous user factory creation for better control over user data.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,16 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::firstOrCreate([
+            'email' => 'mail@syofyanzuhad.dev',
+        ], [
+            'name' => 'Syofyan Zuhad',
+            'password' => bcrypt('password'),
+            'email_verified_at' => now(),
+        ]);
+
+        $this->call([
+            MonitorSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/MonitorSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Monitor;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class MonitorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $monitors = require database_path('seeders/monitors.php');
+
+        // add 1 minute key to each monitor
+        $oneMinuteMonitors = array_map(function ($monitor) {
+            $monitor['uptime_check_interval_in_minutes'] = 1;
+            return $monitor;
+        }, $monitors);
+
+        // add 5 minute key to each monitor
+        // $fiveMinuteMonitors = array_map(function ($monitor) {
+        //     $monitor['uptime_check_interval_in_minutes'] = 5;
+        //     return $monitor;
+        // }, $monitors);
+
+        $monitors = array_merge(
+            $oneMinuteMonitors,
+            // $fiveMinuteMonitors
+        );
+
+        foreach ($monitors as $monitor) {
+            Monitor::firstOrCreate(
+                [
+                    'url' => $monitor['url'],
+                ],
+                [
+                    'uptime_check_interval_in_minutes' => $monitor['uptime_check_interval_in_minutes'],
+                    'uptime_check_enabled' => $monitor['uptime_check_enabled'],
+                    'certificate_check_enabled' => $monitor['certificate_check_enabled'],
+                    'certificate_expiration_date' => $monitor['certificate_expiration_date'] ?? null,
+                ]
+            );
+        }
+    }
+}
