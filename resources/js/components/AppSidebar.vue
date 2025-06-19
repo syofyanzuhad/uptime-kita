fddb2b2f6de74003f38f551469b2e16376db35fd
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-06-19 22:27:21 +0700

    Update AppSidebar to use Github icon for repository link
    - Replaced the Folder icon with the Github icon in the footer navigation items of the AppSidebar component.
    - This change enhances visual clarity and aligns the icon with the purpose of the link, improving user experience.

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem, type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/vue3';
-import { BookOpen, CheckCircle, Folder, LayoutGrid, LucideMonitorSmartphone, SquareActivity } from 'lucide-vue-next';
+import { BookOpen, CheckCircle, Github, LayoutGrid, LucideMonitorSmartphone, SquareActivity } from 'lucide-vue-next';
 import { computed } from 'vue';
 import AppLogo from './AppLogo.vue';
 
@@ -39,7 +39,7 @@ const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
         href: 'https://github.com/syofyanzuhad/uptime-kita',
-        icon: Folder,
+        icon: Github,
     },
     {
         title: 'Documentation',
