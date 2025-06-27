1d241968caf057ec7ee2d51e64417355f7a2010d
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-06-28 00:21:15 +0700

    Add Umami analytics script to app layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -38,6 +38,7 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <script defer src="https://umami.syofyanzuhad.dev/script.js" data-website-id="803a4f91-04d8-43be-9302-82df6ff14481"></script>
 
         @routes
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
