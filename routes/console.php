ea808f2babe20ef0a49d2fab79ed431e036ba07f
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-06-27 19:52:17 +0700

    Add CalculateMonitorUptimeJob for uptime calculation and scheduling
    - Introduced CalculateMonitorUptimeJob to calculate and update monitor uptime percentages based on specified time units (daily, weekly, monthly, yearly).
    - Integrated the job into the console schedule to run hourly for each time unit, enhancing uptime tracking capabilities in the application.

--- routes/console.php
+++ routes/console.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 use Spatie\UptimeMonitor\Commands\CheckUptime;
 use Spatie\UptimeMonitor\Commands\CheckCertificates;
+use App\Jobs\CalculateMonitorUptimeJob;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
@@ -21,3 +22,9 @@ Schedule::command('telescope:prune --hours=48')->everyFiveMinutes();
 
 // === LARAVEL PRUNABLE MODELS ===
 Schedule::command('model:prune')->daily();
+
+Schedule::job(new CalculateMonitorUptimeJob('DAILY'))->hourly();
+Schedule::job(new CalculateMonitorUptimeJob('WEEKLY'))->hourly();
+Schedule::job(new CalculateMonitorUptimeJob('MONTHLY'))->hourly();
+Schedule::job(new CalculateMonitorUptimeJob('YEARLY'))->hourly();
+Schedule::job(new CalculateMonitorUptimeJob('ALL'))->hourly();
