d2a7a051de9d2053ca488a29744a48f89bb5e285
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-06-27 15:57:13 +0700

    Add MonitorResource for API response formatting
    - Introduced MonitorResource class to transform monitor data into a structured JSON response.
    - Included fields such as id, url, uptime status, and certificate details to enhance API usability.

---
+++ app/Http/Resources/MonitorResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MonitorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'url' => $this->raw_url,
+            'uptime_status' => $this->uptime_status,
+            'last_check_date' => $this->uptime_last_check_date,
+            'certificate_check_enabled' => (bool) $this->certificate_check_enabled,
+            'certificate_status' => $this->certificate_status,
+            'certificate_expiration_date' => $this->certificate_expiration_date,
+            'down_for_events_count' => $this->down_for_events_count,
+            'uptime_check_interval' => $this->uptime_check_interval_in_minutes,
+            'is_subscribed' => $this->is_subscribed,
+            'is_public' => $this->is_public,
+        ];
+    }
+}
