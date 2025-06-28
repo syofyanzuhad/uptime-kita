2e5f02e8b0be811e08d0848b841e3caf9d28570c
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-06-28 13:21:26 +0700

    Implement strict model behavior in AppServiceProvider and enforce HTTPS in non-local environments
    - Added strict model behavior configuration to AppServiceProvider, ensuring that Eloquent models enforce strict mode when not in production.
    - Updated boot method to force HTTPS scheme for URLs in non-local environments, enhancing security for application traffic.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Model::shouldBeStrict(! app()->isProduction());
+
         if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
