8800ce779f09e229d1ee901dfa59bddaa7da66b6
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-06-28 13:55:51 +0700

    Refactor PublicMonitorsCard.vue for Improved Styling Consistency
    - Removed unnecessary padding from the certificate status badge class for a cleaner appearance.
    - Enhanced the overall styling of the component to maintain consistency with design standards.

--- resources/js/components/PublicMonitorsCard.vue
+++ resources/js/components/PublicMonitorsCard.vue
@@ -444,7 +444,7 @@ onUnmounted(() => {
                                                     'bg-red-100 text-red-800 dark:bg-red-900/30 dark:text-red-400': monitor.certificate_status === 'invalid',
                                                     'bg-gray-100 text-gray-800 dark:bg-gray-900/30 dark:text-gray-400': monitor.certificate_status === 'not applicable'
                                                 }"
-                                                class="inline-flex uppercase items-center px-2 py-0.5 rounded-full text-xs font-medium"
+                                                class="inline-flex uppercase items-center py-0.5 rounded-full text-xs font-medium"
                                             >
                                                 <span
                                                     v-if="monitor.certificate_status !== undefined"
