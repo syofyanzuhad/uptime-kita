e56b6ebd45a1523650bfecb059899f54a6de9524
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-07-08 11:08:32 +0700

    Refactor AppServiceProvider to include Log Viewer authentication
    - Added LogViewer authentication logic to restrict access to the Log Viewer based on user ID.
    - Reorganized import statements for better clarity and consistency.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,12 @@
 
 namespace App\Providers;
 
-use App\Listeners\SendCustomMonitorNotification;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
+use App\Listeners\SendCustomMonitorNotification;
 use Spatie\UptimeMonitor\Events\UptimeCheckFailed;
 use Spatie\UptimeMonitor\Events\UptimeCheckRecovered;
 
@@ -30,6 +31,8 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
+
+        LogViewer::auth(fn ($request) => auth()->id() === 1);
 
         // Register uptime monitor event listeners
         Event::listen(UptimeCheckFailed::class, SendCustomMonitorNotification::class);
