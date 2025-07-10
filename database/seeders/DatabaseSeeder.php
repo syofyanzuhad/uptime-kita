fc0645a7d0828ebb89f3d12acf95b5d4da8fab8e
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-07-10 11:07:35 +0700

    Comment out MonitorSeeder and NotificationChannelSeeder in DatabaseSeeder to prevent their execution during seeding.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,8 +24,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            MonitorSeeder::class,
-            NotificationChannelSeeder::class,
+            // MonitorSeeder::class,
+            // NotificationChannelSeeder::class,
         ]);
     }
 }
