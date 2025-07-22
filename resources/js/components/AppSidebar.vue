9d662f487d85a2e2612c892ca9fc62d3926816bc
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-07-22 11:03:47 +0700

    Add 'Buy me a coffee' link to AppSidebar component
    - Introduced a new navigation item in the sidebar for users to support the project through a coffee donation.
    - Updated the sidebar icon imports to include the Coffee icon for better visual representation.

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem, type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/vue3';
-import { BookOpen, CheckCircle, Github, LayoutGrid, LucideMonitorSmartphone, SquareActivity } from 'lucide-vue-next';
+import { BookOpen, CheckCircle, Coffee, Github, LayoutGrid, LucideMonitorSmartphone, SquareActivity } from 'lucide-vue-next';
 import { computed } from 'vue';
 import AppLogo from './AppLogo.vue';
 
@@ -36,6 +36,11 @@ const mainNavItems: NavItem[] = [
 ];
 
 const footerNavItems: NavItem[] = [
+    {
+        title: 'Buy me a coffee',
+        href: 'https://www.buymeacoffee.com/syofyanzuhad',
+        icon: Coffee,
+    },
     {
         title: 'Github Repo',
         href: 'https://github.com/syofyanzuhad/uptime-kita',
