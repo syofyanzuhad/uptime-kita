6dd29171a8619ccc34c9dd9bc4ec6bd82b2fbe43
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-07-22 15:37:51 +0700

    Update app title and add meta description for improved SEO

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,7 +30,9 @@
             }
         </style>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Uptime Kita') }}</title>
+        <meta name="description" content="{{ config('app.description', 'Uptime Kita - Simple Uptime Monitoring (open-source)') }}">
+        <meta name="author" content="syofyanzuhad">
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
