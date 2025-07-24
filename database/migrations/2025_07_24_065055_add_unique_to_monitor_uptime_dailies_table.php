b3b89601363784db94478494b9e3da6eea282253
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-07-24 19:10:28 +0700

    Remove redundant checks for unique constraint in monitor_uptime_dailies migration

--- database/migrations/2025_07_24_065055_add_unique_to_monitor_uptime_dailies_table.php
+++ database/migrations/2025_07_24_065055_add_unique_to_monitor_uptime_dailies_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::table('monitor_uptime_dailies', function (Blueprint $table) {
             // Check if the unique constraint already exists
-            if (Schema::hasColumn('monitor_uptime_dailies', 'monitor_id') && Schema::hasColumn('monitor_uptime_dailies', 'date')) {
-                return; // Unique constraint already exists, no need to add it again
-            }
+            // if (Schema::hasColumn('monitor_uptime_dailies', 'monitor_id') && Schema::hasColumn('monitor_uptime_dailies', 'date')) {
+            //     return; // Unique constraint already exists, no need to add it again
+            // }
             $table->unique(['monitor_id', 'date'], 'monitor_uptime_daily_unique');
         });
     }
@@ -27,9 +27,9 @@ return new class extends Migration
     {
         Schema::table('monitor_uptime_dailies', function (Blueprint $table) {
             // Check if the unique constraint exists before dropping it
-            if (!Schema::hasColumn('monitor_uptime_dailies', 'monitor_id') || !Schema::hasColumn('monitor_uptime_dailies', 'date')) {
-                return; // Columns do not exist, no need to drop the constraint
-            }
+            // if (!Schema::hasColumn('monitor_uptime_dailies', 'monitor_id') || !Schema::hasColumn('monitor_uptime_dailies', 'date')) {
+            //     return; // Columns do not exist, no need to drop the constraint
+            // }
             // Drop the unique constraint on monitor_id and date
             $table->dropUnique('monitor_uptime_daily_unique');
         });
