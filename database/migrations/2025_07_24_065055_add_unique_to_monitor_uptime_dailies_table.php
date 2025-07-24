31d1f5106d13cdf73478c15ffae0d68c998c91ee
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-07-24 19:35:07 +0700

    Fix condition to check for column existence before dropping unique constraint in migration

--- database/migrations/2025_07_24_065055_add_unique_to_monitor_uptime_dailies_table.php
+++ database/migrations/2025_07_24_065055_add_unique_to_monitor_uptime_dailies_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
     {
         Schema::table('monitor_uptime_dailies', function (Blueprint $table) {
             // Check if the unique constraint exists before dropping it
-            if (!Schema::hasColumn('monitor_uptime_dailies', 'monitor_id') || !Schema::hasColumn('monitor_uptime_dailies', 'date')) {
+            if (Schema::hasColumn('monitor_uptime_dailies', 'monitor_id') || Schema::hasColumn('monitor_uptime_dailies', 'date')) {
                 return; // Columns do not exist, no need to drop the constraint
             }
             // Drop the unique constraint on monitor_id and date
