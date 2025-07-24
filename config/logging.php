a03703662d94f31f8f9b240e709202b5bd803f27
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-07-24 11:39:46 +0700

    Add configuration for larabug error logging and update logging settings

--- config/logging.php
+++ config/logging.php
@@ -89,7 +89,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
@@ -127,6 +127,9 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
+        'larabug' => [
+            'driver' => 'larabug',
+        ],
     ],
 
 ];
