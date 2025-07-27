55c7ea976a08cd62920db7801767f8a8ac2e5206
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-07-27 12:50:26 +0700

    Remove FlashMessage component from Index.vue: Clean up the UI by eliminating unnecessary component for improved layout.

--- resources/js/pages/uptime/Index.vue
+++ resources/js/pages/uptime/Index.vue
@@ -154,7 +154,6 @@ function onPaginationLinkClick(link: PaginatorLink) {
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <FlashMessage />
 
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
                 <div class="flex justify-between items-center mb-4">
