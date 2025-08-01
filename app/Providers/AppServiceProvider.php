720a007840684fd06aa5ee030270769c5a084493
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-08-01 10:58:58 +0700

    Add health checks to AppServiceProvider: Register UsedDiskSpaceCheck and DatabaseCheck for enhanced health monitoring in the application.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,16 @@
 
 namespace App\Providers;
 
+use Spatie\Health\Facades\Health;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 use Opcodes\LogViewer\Facades\LogViewer;
+use Spatie\Health\Checks\Checks\DatabaseCheck;
 use App\Listeners\SendCustomMonitorNotification;
 use Spatie\UptimeMonitor\Events\UptimeCheckFailed;
+use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
 use Spatie\UptimeMonitor\Events\UptimeCheckRecovered;
 
 class AppServiceProvider extends ServiceProvider
@@ -37,5 +40,10 @@ class AppServiceProvider extends ServiceProvider
         // Register uptime monitor event listeners
         Event::listen(UptimeCheckFailed::class, SendCustomMonitorNotification::class);
         Event::listen(UptimeCheckRecovered::class, SendCustomMonitorNotification::class);
+
+        Health::checks([
+            UsedDiskSpaceCheck::new(),
+            DatabaseCheck::new(),
+        ]);
     }
 }
