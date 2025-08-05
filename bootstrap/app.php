034f73f57a451f87984cda55cdb19eafa17bf5c8
Syofyan Zuhad 🇵🇸 <sofyanzuhad2@gmail.com>
2025-08-05 12:27:22 +0700

    fix: csrf exception for webhook

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,7 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->validateCsrfTokens(except: [
-            '/telegram/webhook',
+            '/webhook/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
