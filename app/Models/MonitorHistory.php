e16cc79f4abad8869ed7b25ff4569d50f2868264
syofyanzuhad <mail@syofyanzuhad.dev>
2025-08-26 15:03:15 +0700

    Enhance MonitorHistory model with factory support
    - Added HasFactory trait to the MonitorHistory model to enable factory-based testing.
    - Introduced MonitorHistoryFactory for generating test data, including fields like monitor_id, uptime_status, message, response_time, status_code, and checked_at.

--- app/Models/MonitorHistory.php
+++ app/Models/MonitorHistory.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Prunable;
 
 class MonitorHistory extends Model
 {
-    use Prunable;
+    use HasFactory, Prunable;
 
     protected $fillable = [
         'monitor_id',
