3f07018b70c2ba6c30c924c9ebca998373721154
syofyanzuhad <mail@syofyanzuhad.dev>
2026-04-23 01:23:29 +0700

    feat: add cloudflare web analytics script

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -63,6 +63,7 @@
         @if(config('app.env') !== 'local')
             <script defer src="https://umami.syofyanzuhad.dev/script.js" data-website-id="803a4f91-04d8-43be-9302-82df6ff14481"></script>
             <script defer src="https://cloud.umami.is/script.js" data-website-id="27ccc29a-1870-4f9f-9d22-9fd9f91e9b12"></script>
+            <!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "bcba406328054989b8d6bbf513b4197f"}'></script><!-- End Cloudflare Web Analytics -->
         @endif
         @routes
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
